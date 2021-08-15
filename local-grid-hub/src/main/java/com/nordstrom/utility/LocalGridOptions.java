578e91da3bc8b76249607d7387fb5d965a10e094
Scott Babcock <scoba@hotmail.com>
2021-08-15 15:05:30 -0700

    Save current state

---
+++ local-grid-hub/src/main/java/com/nordstrom/utility/LocalGridOptions.java
@@ -0,0 +1,92 @@
+package com.nordstrom.utility;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.converters.PathConverter;
+import com.nordstrom.automation.selenium.AbstractSeleniumConfig.SeleniumSettings;
+import com.nordstrom.automation.selenium.exceptions.GridServerLaunchFailedException;
+import com.nordstrom.automation.selenium.plugins.PhantomJsPlugin;
+
+public class LocalGridOptions {
+
+    @Parameter(names = "-port", description = "Port for local hub server")
+    private Integer port;
+
+    @Parameter(names = "-plugins", description = "Semicolon-delimited list of fully-qualified node plugin classes")
+    private String plugins = PhantomJsPlugin.class.getName();
+
+    @Parameter(names = "-shutdown", description = "Shutdown active local Grid")
+    private boolean shutdown = false;
+
+    @Parameter(names = "-workingDir", description = "Working directory for servers", converter = PathConverter.class)
+    private Path workingDir;
+
+    @Parameter(names = "-logsFolder", description = "Server output logs folder", converter = PathConverter.class)
+    private Path logsFolder;
+
+    @Parameter(names = "-noRedirect", description = "Disable server output redirection")
+    private Boolean noRedirect;
+
+    @Parameter(names = { "-help", "-?" }, description = "Display supported options and exit", help = true)
+    private boolean help = false;
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public String getPlugins() {
+        return plugins;
+    }
+
+    public boolean doShutdown() {
+        return shutdown;
+    }
+
+    public Path getWorkingDir() {
+        return workingDir;
+    }
+
+    public Path getLogsFolder() {
+        return logsFolder;
+    }
+
+    public boolean noRedirect() {
+        return (noRedirect != null) ? noRedirect : false;
+    }
+
+    public void setHelp() {
+        help = true;
+    }
+
+    public boolean isHelp() {
+        return help;
+    }
+
+    public void injectSettings() {
+        String workingFolder = System.getProperty("user.dir");
+        if (port != null) {
+            System.setProperty(SeleniumSettings.HUB_PORT.key(), port.toString());
+        }
+        if (workingDir != null) {
+            workingFolder = workingDir.toString();
+            System.setProperty(SeleniumSettings.GRID_WORKING_DIR.key(), workingFolder);
+        }
+        if (logsFolder != null) {
+            System.setProperty(SeleniumSettings.GRID_LOGS_FOLDER.key(), logsFolder.toString());
+        }
+        if (noRedirect != null) {
+            System.setProperty(SeleniumSettings.GRID_NO_REDIRECT.key(), noRedirect.toString());
+        }
+        Path workingPath = Paths.get(workingFolder);
+        try {
+            if (!workingPath.toFile().exists()) {
+                Files.createDirectories(workingPath);
+            }
+        } catch (IOException e) {
+            throw new GridServerLaunchFailedException("parent", e);
+        }
+    }
+}
---
+++ local-grid-hub/src/main/java/com/nordstrom/utility/Main.java
@@ -0,0 +1,57 @@
+package com.nordstrom.utility;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+import com.nordstrom.automation.selenium.SeleniumConfig;
+import com.nordstrom.automation.selenium.AbstractSeleniumConfig.SeleniumSettings;
+import com.nordstrom.automation.selenium.core.GridUtility;
+import com.nordstrom.automation.selenium.core.SeleniumGrid;
+
+public class Main {
+    public static void main(String... args)
+            throws InterruptedException, ConfigurationException, IOException, TimeoutException {
+        LocalGridOptions opts = new LocalGridOptions();
+        JCommander parser = new JCommander(opts);
+        try {
+            parser.parse(args);
+        } catch (ParameterException e) {
+            JCommander.getConsole().println(e.getMessage());
+            opts.setHelp();
+        }
+
+        if (opts.isHelp()) {
+            parser.setProgramName("local-grid-hub");
+            parser.usage();
+            return;
+        }
+
+        opts.injectSettings();
+
+        SeleniumConfig config = new SeleniumConfig();
+
+        URL hubUrl = config.getHubUrl();
+        if (hubUrl == null) {
+            int hubPort = config.getInt(SeleniumSettings.HUB_PORT.key());
+            String hostStr = "http://" + GridUtility.getLocalHost() + ":" + hubPort;
+            hubUrl = new URL(hostStr);
+        }
+
+        boolean isActive = GridUtility.isHubActive(hubUrl);
+
+        if (opts.doShutdown() && isActive) {
+            SeleniumGrid.create(config, hubUrl).shutdown(true);
+        } else {
+            if (!isActive) {
+                SeleniumGrid grid = SeleniumGrid.create(config, hubUrl);
+                hubUrl = grid.getHubServer().getUrl();
+            }
+            JCommander.getConsole().println(hubUrl.toString());
+        }
+    }
+}
