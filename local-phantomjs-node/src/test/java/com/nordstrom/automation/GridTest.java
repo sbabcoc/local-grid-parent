494a1579536a2cb3e7b98a4b8418e10c307fad0d
Scott Babcock <scoba@hotmail.com>
2021-09-18 01:22:53 -0700

    Get test mostly functioning

--- local-phantomjs-node/src/test/java/com/nordstrom/automation/GridTest.java
+++ local-phantomjs-node/src/test/java/com/nordstrom/automation/GridTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.URI;
+import java.net.URL;
 import java.util.concurrent.TimeoutException;
 
 import org.junit.Test;
@@ -15,24 +16,58 @@ import org.openqa.selenium.net.PortProber;
 import org.openqa.selenium.os.CommandLine;
 import com.google.common.collect.ObjectArrays;
 import com.nordstrom.automation.selenium.SeleniumConfig;
+import com.nordstrom.automation.selenium.AbstractSeleniumConfig.SeleniumSettings;
 import com.nordstrom.automation.selenium.annotations.InitialPage;
 import com.nordstrom.automation.selenium.core.GridUtility;
 import com.nordstrom.automation.selenium.core.SeleniumGrid;
 import com.nordstrom.automation.selenium.examples.ExamplePage;
-import com.nordstrom.automation.selenium.examples.JUnitRoot;
 import com.nordstrom.automation.selenium.interfaces.DriverProvider;
+import com.nordstrom.automation.selenium.junit.JUnitBase;
 import com.nordstrom.automation.selenium.plugins.PhantomJsPlugin;
 import com.nordstrom.common.base.UncheckedThrow;
 import com.nordstrom.common.jar.JarUtils;
 import com.nordstrom.utility.LocalGridOptions;
 
 @InitialPage(ExamplePage.class)
-public class GridTest extends JUnitRoot implements DriverProvider {
+public class GridTest extends JUnitBase implements DriverProvider {
     
-    private static SeleniumGrid grid = null;
-    private static final PhantomJsPlugin driverPlugin = new PhantomJsPlugin();
-    private static final SeleniumConfig config = SeleniumConfig.getConfig();
+    private static final SeleniumConfig config;
+    private static final PhantomJsPlugin driverPlugin;
+    private static final SeleniumGrid grid;
     
+    static {
+        driverPlugin = new PhantomJsPlugin();
+        config = SeleniumConfig.getConfig();
+        
+        String[] dependencyContexts =
+                ObjectArrays.concat(LocalGridOptions.class.getName(), driverPlugin.getDependencyContexts());
+        
+        String[] argsList = new String[] {
+                "-cp", JarUtils.getClasspath(dependencyContexts),
+                "com.nordstrom.utility.Main",
+                "-port", Integer.toString(PortProber.findFreePort()),
+                "-plugins", driverPlugin.getClass().getName()
+        };
+        
+        String executable = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+        CommandLine process = new CommandLine(executable, argsList);
+        process.execute();
+        
+        try {
+            String stdOut = process.getStdOut();
+            String[] lines = stdOut.split("[\\n\\r]+");
+            String hubHost = lines[lines.length - 1];
+            URI hubUri = URI.create(hubHost);
+            config.setTargetUri(hubUri);
+            URL hubUrl = hubUri.toURL();
+            grid = SeleniumGrid.create(config, hubUrl);
+            System.setProperty(SeleniumSettings.HUB_HOST.key(), hubUrl.toString());
+            System.setProperty(SeleniumSettings.HUB_PORT.key(), Integer.toString(hubUrl.getPort()));
+        } catch (IOException | InterruptedException | TimeoutException e) {
+            throw UncheckedThrow.throwUnchecked(e);
+        }
+    }
+
     @Test
     public void test() {
         ExamplePage page = this.getInitialPage();
@@ -41,33 +76,8 @@ public class GridTest extends JUnitRoot implements DriverProvider {
 
     @Override
     public WebDriver provideDriver(Method method) {
-        Capabilities capabilities = getGrid().getPersonality(config, driverPlugin.getBrowserName());
-        return GridUtility.getDriver(getGrid().getHubServer().getUrl(), capabilities);
+        Capabilities capabilities = grid.getPersonality(config, driverPlugin.getBrowserName());
+        return GridUtility.getDriver(grid.getHubServer().getUrl(), capabilities);
     }
     
-    private static synchronized SeleniumGrid getGrid() {
-        if (grid == null) {
-            String[] dependencyContexts =
-                    ObjectArrays.concat(LocalGridOptions.class.getName(), driverPlugin.getDependencyContexts());
-            
-            String[] argsList = new String[] {
-                    "-cp", JarUtils.getClasspath(dependencyContexts),
-                    "com.nordstrom.utility.Main",
-                    "-port", Integer.toString(PortProber.findFreePort()),
-                    "-plugins", driverPlugin.getClass().getName()
-            };
-            
-            String executable = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
-            CommandLine process = new CommandLine(executable, argsList);
-            process.execute();
-            
-            try {
-                grid = SeleniumGrid.create(config, URI.create(process.getStdOut()).toURL());
-            } catch (IOException | InterruptedException | TimeoutException e) {
-                throw UncheckedThrow.throwUnchecked(e);
-            }
-        }
-        return grid;
-    }
-
 }
