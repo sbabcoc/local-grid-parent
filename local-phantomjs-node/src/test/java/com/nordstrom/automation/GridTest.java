a48ec256cfcce482699b416afc723724e2298e53
Scott Babcock <scoba@hotmail.com>
2021-09-19 01:22:32 -0700

    Get unit test working

--- local-phantomjs-node/src/test/java/com/nordstrom/automation/GridTest.java
+++ local-phantomjs-node/src/test/java/com/nordstrom/automation/GridTest.java
@@ -3,81 +3,96 @@ package com.nordstrom.automation;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Method;
 import java.net.URI;
-import java.net.URL;
-import java.util.concurrent.TimeoutException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.net.PortProber;
 import org.openqa.selenium.os.CommandLine;
 import com.google.common.collect.ObjectArrays;
-import com.nordstrom.automation.selenium.SeleniumConfig;
 import com.nordstrom.automation.selenium.AbstractSeleniumConfig.SeleniumSettings;
+import com.nordstrom.automation.selenium.DriverPlugin;
+import com.nordstrom.automation.selenium.SeleniumConfig;
 import com.nordstrom.automation.selenium.annotations.InitialPage;
-import com.nordstrom.automation.selenium.core.GridUtility;
-import com.nordstrom.automation.selenium.core.SeleniumGrid;
+import com.nordstrom.automation.selenium.core.DriverManager;
 import com.nordstrom.automation.selenium.examples.ExamplePage;
-import com.nordstrom.automation.selenium.interfaces.DriverProvider;
 import com.nordstrom.automation.selenium.junit.JUnitBase;
+import com.nordstrom.automation.selenium.model.Page;
 import com.nordstrom.automation.selenium.plugins.PhantomJsPlugin;
-import com.nordstrom.common.base.UncheckedThrow;
 import com.nordstrom.common.jar.JarUtils;
 import com.nordstrom.utility.LocalGridOptions;
 
 @InitialPage(ExamplePage.class)
-public class GridTest extends JUnitBase implements DriverProvider {
+public class GridTest extends JUnitBase {
     
-    private static final SeleniumConfig config;
-    private static final PhantomJsPlugin driverPlugin;
-    private static final SeleniumGrid grid;
+    private static final DriverPlugin driverPlugin;
+    private static final URI hubUri;
+    private static final URI targetUri;
     
     static {
         driverPlugin = new PhantomJsPlugin();
-        config = SeleniumConfig.getConfig();
         
         String[] dependencyContexts =
                 ObjectArrays.concat(LocalGridOptions.class.getName(), driverPlugin.getDependencyContexts());
         
-        String[] argsList = new String[] {
-                "-cp", JarUtils.getClasspath(dependencyContexts),
-                "com.nordstrom.utility.Main",
-                "-port", Integer.toString(PortProber.findFreePort()),
-                "-plugins", driverPlugin.getClass().getName()
-        };
+        List<String> argsList = new ArrayList<>();
+        
+        // get assembled classpath string
+        String classPath = JarUtils.getClasspath(dependencyContexts);
+        // split on Java agent list separator
+        String[] pathBits = classPath.split("\n");
+        // if agent(s) specified
+        if (pathBits.length > 1) {
+            // extract classpath
+            classPath = pathBits[0];
+            // for each specified agent...
+            for (String agentPath : pathBits[1].split("\t")) {
+                // ... specify a 'javaagent' argument
+                argsList.add(0, "-javaagent:" + agentPath);
+            }
+        }
+        
+        argsList.add("-cp");
+        argsList.add(classPath);
+        argsList.add("com.nordstrom.utility.Main");
+        argsList.add("-port");
+        argsList.add(Integer.toString(PortProber.findFreePort()));
+        argsList.add("-plugins");
+        argsList.add(driverPlugin.getClass().getName());
         
         String executable = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
-        CommandLine process = new CommandLine(executable, argsList);
+        CommandLine process = new CommandLine(executable, argsList.toArray(new String[0]));
         process.execute();
         
-        try {
-            String stdOut = process.getStdOut();
-            String[] lines = stdOut.split("[\\n\\r]+");
-            String hubHost = lines[lines.length - 1];
-            URI hubUri = URI.create(hubHost);
-            config.setTargetUri(hubUri);
-            URL hubUrl = hubUri.toURL();
-            grid = SeleniumGrid.create(config, hubUrl);
-            System.setProperty(SeleniumSettings.HUB_HOST.key(), hubUrl.toString());
-            System.setProperty(SeleniumSettings.HUB_PORT.key(), Integer.toString(hubUrl.getPort()));
-        } catch (IOException | InterruptedException | TimeoutException e) {
-            throw UncheckedThrow.throwUnchecked(e);
-        }
+        String[] lines = process.getStdOut().split("[\\n\\r]+");
+        hubUri = URI.create(lines[lines.length - 1]);
+        
+        System.setProperty(SeleniumSettings.HUB_HOST.key(), hubUri.toString());
+        System.setProperty(SeleniumSettings.HUB_PORT.key(), Integer.toString(hubUri.getPort()));
+        System.setProperty(SeleniumSettings.BROWSER_NAME.key(), driverPlugin.getBrowserName());
+        
+        targetUri = ExamplePage.setHubAsTarget();
+    }
+    
+    @Before
+    public void beforeTest() {
+        DriverManager.injectDriver(this, null);
     }
 
     @Test
     public void test() {
-        ExamplePage page = this.getInitialPage();
+        InitialPage initialPage = this.getClass().getAnnotation(InitialPage.class);
+        ExamplePage page = Page.openInitialPage(initialPage, getDriver(), targetUri);
         assertEquals(ExamplePage.TITLE, page.getTitle());
     }
-
-    @Override
-    public WebDriver provideDriver(Method method) {
-        Capabilities capabilities = grid.getPersonality(config, driverPlugin.getBrowserName());
-        return GridUtility.getDriver(grid.getHubServer().getUrl(), capabilities);
-    }
     
+    @After
+    public void afterTest() throws InterruptedException {
+        DriverManager.closeDriver(this);
+        SeleniumConfig.getConfig().getSeleniumGrid().shutdown(true);
+    }
+
 }
