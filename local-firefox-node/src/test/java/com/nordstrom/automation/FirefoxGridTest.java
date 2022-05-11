337a357f9bbb8a8f75cb499320ab94e036d7bf36
Scott Babcock <scoba@hotmail.com>
2022-05-11 15:59:38 -0700

    Remove overrides (shadow DOM now works)

--- local-firefox-node/src/test/java/com/nordstrom/automation/FirefoxGridTest.java
+++ local-firefox-node/src/test/java/com/nordstrom/automation/FirefoxGridTest.java
@@ -3,9 +3,6 @@ package com.nordstrom.automation;
 import java.io.File;
 import java.util.Objects;
 
-import org.junit.Ignore;
-import org.junit.Test;
-
 import com.nordstrom.automation.selenium.core.SeleniumGrid;
 import com.nordstrom.automation.selenium.plugins.FirefoxPlugin;
 import com.nordstrom.automation.selenium.utility.BinaryFinder;
@@ -15,26 +12,6 @@ import com.nordstrom.utility.GridLauncher;
 public class FirefoxGridTest extends AbstractGridTest {
     
     private static final String PATH_PROPERTY = "webdriver.gecko.driver";
-    
-    @Test
-    @Ignore
-    @Override
-    public void testShadowRootByLocator() { }
-
-    @Test
-    @Ignore
-    @Override
-    public void testShadowRootByElement() { }
-    
-    @Test
-    @Ignore
-    @Override
-    public void testShadowRootList() { }
-    
-    @Test
-    @Ignore
-    @Override
-    public void testShadowRootMap() { }
     
     public static SeleniumGrid launchGrid() {
         File driverPath = Objects.requireNonNull(
