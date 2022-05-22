e6bbb44563c03c5d9acc1d0c88a2b1e37fde0296
Scott Babcock <scoba@hotmail.com>
2022-05-22 00:20:22 -0700

    Upgrade foundation; remove driver executable search

--- local-opera-node/src/test/java/com/nordstrom/automation/OperaGridTest.java
+++ local-opera-node/src/test/java/com/nordstrom/automation/OperaGridTest.java
@@ -1,29 +1,12 @@
 package com.nordstrom.automation;
 
-import java.io.File;
-import java.util.Objects;
-
 import com.nordstrom.automation.selenium.DriverPlugin;
-import com.nordstrom.automation.selenium.core.SeleniumGrid;
 import com.nordstrom.automation.selenium.plugins.OperaPlugin;
-import com.nordstrom.automation.selenium.utility.BinaryFinder;
 import com.nordstrom.utility.AbstractGridTest;
-import com.nordstrom.utility.GridLauncher;
 
 public class OperaGridTest extends AbstractGridTest {
     
-    private static final String PATH_PROPERTY = "webdriver.opera.driver";
-
     private DriverPlugin plugin = new OperaPlugin();
-
-    @Override
-    public SeleniumGrid launchGrid() {
-        File driverPath = Objects.requireNonNull(
-                BinaryFinder.findBinary("operadriver", PATH_PROPERTY, null, null),
-                "Executable 'operadriver' not found");
-        System.setProperty(PATH_PROPERTY, driverPath.getAbsolutePath());
-        return GridLauncher.launch(plugin);
-    }
 
     @Override
     public DriverPlugin getPlugin() {
