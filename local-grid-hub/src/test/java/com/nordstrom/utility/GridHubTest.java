164f230851d4c960d17bb18224b7175b6625e67b
Scott Babcock <scoba@hotmail.com>
2022-05-22 20:40:22 -0700

    Configure Surefire to skip tests; add hub test

--- local-grid-hub/src/test/java/com/nordstrom/utility/GridHubTest.java
+++ local-grid-hub/src/test/java/com/nordstrom/utility/GridHubTest.java
@@ -1,11 +1,49 @@
 package com.nordstrom.utility;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Map;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.json.Json;
+import com.nordstrom.automation.selenium.core.GridUtility;
 import com.nordstrom.automation.selenium.core.SeleniumGrid;
 
 public class GridHubTest {
 
-    public static SeleniumGrid launchGrid() {
-        return GridLauncher.launch(null);
+    private static SeleniumGrid seleniumGrid = null;
+    private static final String HUB_QUERY = "/grid/api/hub";
+    
+    @BeforeClass
+    public static void beforeClass() {
+        seleniumGrid = GridLauncher.launch();
     }
 
+    @Test
+    public void testBasicPage() throws IOException {
+        URL hubUrl = seleniumGrid.getHubServer().getUrl();
+        String json = queryHub(hubUrl);
+        Map<String, Object> response = new Json().toType(json, Map.class);
+        assertTrue((boolean) response.get("success"));
+    }
+    
+    private static String queryHub(URL hubUrl) throws IOException {
+        String json;
+        String url = hubUrl.getProtocol() + "://" + hubUrl.getAuthority() + HUB_QUERY;
+        try (InputStream is = new URL(url).openStream()) {
+            json = GridUtility.readAvailable(is);
+        }
+        return json;
+    }
+    
+    @AfterClass
+    public static void afterClass() throws InterruptedException {
+        seleniumGrid.shutdown(true);
+    }
+    
 }
