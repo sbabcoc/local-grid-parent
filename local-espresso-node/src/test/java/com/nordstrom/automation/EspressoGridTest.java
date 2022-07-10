90e04110f40dd1c686240f373e3147767f4da269
Scott Babcock <scoba@hotmail.com>
2022-07-09 21:07:10 -0700

    Add support for Appium Espresso node

---
+++ local-espresso-node/src/test/java/com/nordstrom/automation/EspressoGridTest.java
@@ -0,0 +1,49 @@
+package com.nordstrom.automation;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import com.nordstrom.automation.selenium.DriverPlugin;
+import com.nordstrom.automation.selenium.annotations.InitialPage;
+import com.nordstrom.automation.selenium.core.SeleniumGrid;
+import com.nordstrom.automation.selenium.examples.AndroidPage;
+import com.nordstrom.automation.selenium.examples.ExamplePage;
+import com.nordstrom.automation.selenium.junit.JUnitBase;
+import com.nordstrom.automation.selenium.plugins.EspressoPlugin;
+import com.nordstrom.utility.GridLauncher;
+
+@InitialPage(AndroidPage.class)
+public class EspressoGridTest extends JUnitBase {
+    
+    private static SeleniumGrid seleniumGrid = null;
+    private DriverPlugin plugin = new EspressoPlugin();
+
+    @Before
+    public void beforeTest() {
+        launchSeleniumGrid();
+        ExamplePage.setHubAsTarget();
+    }
+
+    @Test
+    public void testSearchActivity() {
+        AndroidPage page = getInitialPage();
+        page.submitSearchQuery("Hello world!");
+        assertEquals(page.getSearchResult(), "Hello world!");
+    }
+
+    private void launchSeleniumGrid() {
+        if (seleniumGrid == null) {
+            seleniumGrid = launchGrid();
+        }
+    }
+    
+    private SeleniumGrid launchGrid() {
+        return GridLauncher.launch(getPlugin());
+    }
+    
+    private DriverPlugin getPlugin() {
+        return plugin;
+    }
+    
+}
